c029f86cbfd230984f3211a53cd3eb90f44870b0
Taufiq Husada Daryanto <13518058@std.stei.itb.ac.id>
2020-11-18 15:48:43 +0700

    init

---
+++ src/net/codejava/Hello.java
@@ -0,0 +1,16 @@
+package net.codejava;
+ 
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
+ 
+@WebService
+@SOAPBinding(style = Style.RPC)
+public class Hello {
+ 
+    @WebMethod
+    public String bonjour(String name) {
+        return String.format("Hello %s", name);
+    }
+}
