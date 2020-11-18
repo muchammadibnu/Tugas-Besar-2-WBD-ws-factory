0fcf6edb53a8c7ccce1ae428d8b8bb2aca97ee5d
Taufiq Husada Daryanto <13518058@std.stei.itb.ac.id>
2020-11-18 21:44:26 +0700

    modif addNewChocolateRecipe

--- src/net/codejava/Chocolate.java
+++ src/net/codejava/Chocolate.java
@@ -15,8 +15,8 @@ import javax.jws.soap.SOAPBinding.Style;
 @SOAPBinding(style = Style.RPC)
 public class Chocolate {
 	@WebMethod
-	public String addNewChocolateRecipe(String chocolateName, String strListBahan, String strListJumlah) {
-		// input nya: "nama coklat" , "bahan 1, bahan 2, .." , "jumlah bahan 1, jumlah bahan 2, ..."
+	public String addNewChocolateRecipe(String chocolateName, String strListBahan, String strListJumlah, int hargaCoklat) {
+		// input nya: "nama coklat" , "bahan 1, bahan 2, .." , "jumlah bahan 1, jumlah bahan 2, ..." , harga
 		
 		List<String> listBahan = Arrays.asList(strListBahan.split(","));
 		List<String> listJumlah = Arrays.asList(strListJumlah.split(","));
@@ -26,8 +26,11 @@ public class Chocolate {
 		try {
 			conn = dbConnector.getConnection();
 			
+			String query = "INSERT INTO harga_coklat (name, harga) VALUES ('" + chocolateName + "'," + hargaCoklat + ")";
+			dbConnector.executeUpdate(conn, query);
+			
 			for (int i = 0; i<listBahan.size(); ++i) {
-				String query = "INSERT INTO resep (chocolate_name, bahan, jumlah) VALUES ('" + chocolateName + "', '" + listBahan.get(i) + "', " + listJumlah.get(i) + ")";
+				query = "INSERT INTO resep (chocolate_name, bahan, jumlah) VALUES ('" + chocolateName + "', '" + listBahan.get(i) + "', " + listJumlah.get(i) + ")";
 				dbConnector.executeUpdate(conn, query);
 			}
 			return "success";
