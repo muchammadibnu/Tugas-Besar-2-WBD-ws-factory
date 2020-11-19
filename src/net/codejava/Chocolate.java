79df0b9dcd2d2300210c3d144974aa269985d4c0
Taufiq Husada Daryanto <13518058@std.stei.itb.ac.id>
2020-11-19 08:36:04 +0700

    add getListRecipe

--- src/net/codejava/Chocolate.java
+++ src/net/codejava/Chocolate.java
@@ -1,7 +1,9 @@
 package net.codejava;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Arrays;
 import java.util.List;
 
@@ -41,16 +43,33 @@ public class Chocolate {
 	}
 	
 	@WebMethod
-	public String addBahanAndHarga(String name, int harga) {
+	public String getListRecipe() {
 		DbConnector dbConnector = new DbConnector();
 		Connection conn = null;
 		try {
+			String strHtml = "";
+			
 			conn = dbConnector.getConnection();
-			
-			String query = "INSERT INTO harga_bahan (name, harga) VALUES ('" + name + "', " + harga + ")";
-			dbConnector.executeUpdate(conn, query);
-			
-			return "success";
+	    	Statement stmt = conn.createStatement();
+	    	// get list name chocolate
+	    	ResultSet rs = stmt.executeQuery("SELECT name FROM harga_coklat");
+	    	while ( rs.next() ) {
+	    		String chocolate_name = rs.getString("name");
+	    		strHtml += "<h2>" + chocolate_name + "</h2>";
+	    		strHtml += "<p> List Bahan </p>";
+	    		strHtml += "<ul>";
+	    		Statement stmt2 = conn.createStatement();
+	    		ResultSet rs2 = stmt2.executeQuery("SELECT bahan, jumlah FROM resep where chocolate_name='" + chocolate_name + "'");
+	    		while(rs2.next()) {
+	    			String bahan = rs2.getString("bahan");
+	    			int jumlah = rs2.getInt("jumlah");
+	    			strHtml += "<li>" + bahan + " : " + jumlah + "</li>";
+	    		}
+	    		strHtml += "</ul>";
+	    		stmt2.close();
+	    	}
+	    	stmt.close();
+	    	return strHtml;
 			
 		} catch (SQLException e) {
 			return e.getMessage();
